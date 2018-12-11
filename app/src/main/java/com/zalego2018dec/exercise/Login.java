1f3ca095067c1627709599ff811e7ebc38cc4603
royWati <royywati@gmail.com>
2018-12-11 11:53:47 +0300

    calebs project

---
+++ app/src/main/java/com/zalego2018dec/exercise/Login.java
@@ -0,0 +1,19 @@
+package com.zalego2018dec.exercise;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by folio on 12/10/2018.
+ */
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+        setTitle("Caleb's project");
+    }
+}
